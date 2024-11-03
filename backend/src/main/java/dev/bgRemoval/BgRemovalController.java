9b5206f80cb97cd395a7f46f1c9746a9f13b3ef5
Artur Yurchenko <artur24814@gmail.com>
2024-11-03 16:32:43 +0100

    First test for bgRemovalController has been added (Image input -> image output)

--- backend/src/main/java/dev/bgRemoval/BgRemovalController.java
+++ backend/src/main/java/dev/bgRemoval/BgRemovalController.java
@@ -16,15 +16,13 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 
 @RestController
 @RequestMapping("/api/bg-remove")
-public class bgRemovalController {
+public class BgRemovalController {
 
-    private static final Logger logger = LoggerFactory.getLogger(bgRemovalController.class);
+    private static final Logger logger = LoggerFactory.getLogger(src.main.java.dev.bgRemoval.BgRemovalController.class);
 
     @PostMapping("/process")
     public ResponseEntity<byte[]> process(@RequestParam("file") MultipartFile file) throws IOException {
---
+++ backend/src/test/java/bgRemoval/BgRemovalControllerTest.java
@@ -0,0 +1,50 @@
+package bgRemoval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import src.main.java.dev.App;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = App.class)
+@AutoConfigureMockMvc
+public class BgRemovalControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    private void shouldReturnImage_whenImageIsUploaded() throws Exception {
+        BufferedImage bufferedImage = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        ImageIO.write(bufferedImage, "png", baos);
+        byte[] imageBytes = baos.toByteArray();
+
+        MockMultipartFile mockMultipartFile = new MockMultipartFile(
+            "file",
+            "test.png",
+            MediaType.IMAGE_PNG_VALUE,
+            imageBytes
+        );
+
+        mockMvc.perform(MockMvcRequestBuilders.multipart("/api/bg-remove/process")
+                .file(mockMultipartFile))
+            .andExpect(status().isOk())
+            .andExpect(MockMvcResultMatchers.header().string("Content-Type", "image/png"))
+            .andExpect(content().bytes(imageBytes));
+    }
+}
