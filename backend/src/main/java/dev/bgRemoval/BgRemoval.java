4fe5205f1d7fbc2e0829a7e8354750e8c8d9ccf9
Artur Yurchenko <artur24814@gmail.com>
2024-11-04 11:16:07 +0100

    First version of BgRemoval class has been done (Require improving)

--- backend/src/main/java/dev/bgRemoval/BgRemoval.java
+++ backend/src/main/java/dev/bgRemoval/BgRemoval.java
@@ -1,11 +1,7 @@
 package src.main.java.dev.bgRemoval;
 
-import org.bytedeco.opencv.opencv_core.Mat;
-import org.bytedeco.opencv.opencv_core.MatVector;
-import org.bytedeco.opencv.opencv_core.Rect;
-import org.bytedeco.opencv.opencv_core.Scalar;
+import org.bytedeco.opencv.opencv_core.*;
 import org.bytedeco.opencv.global.opencv_core;
-import org.bytedeco.opencv.global.opencv_imgcodecs;
 import org.bytedeco.opencv.global.opencv_imgproc;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,50 +13,57 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import javax.imageio.ImageIO;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class BgRemoval {
     private static final Logger logger = LoggerFactory.getLogger(src.main.java.dev.bgRemoval.BgRemoval.class);
 
     public byte[] processedImage(MultipartFile file) throws IOException {
-        Mat processedImage = extractLargeObjects(file);
+        Mat processedImage = removeBackground(file);
         return matToByteArray(processedImage);
     }
 
-    private Mat extractLargeObjects(MultipartFile file) throws IOException {
+    public Mat removeBackground(MultipartFile file) throws IOException {
         Mat image = multipartFileToMat(file);
 
-        // Krok 1: Konwersja do skali szarości
-        Mat gray = new Mat();
-        opencv_imgproc.cvtColor(image, gray, opencv_imgproc.COLOR_BGR2GRAY);
+        Mat mask = getMaskForGrabCut(image);
+        Rect rect = getRectSurroundingObjectInTheCenterImage(image);
 
-        // Krok 2: Rozmycie Gaussa dla usunięcia szumów
-        opencv_imgproc.GaussianBlur(gray, gray, new org.bytedeco.opencv.opencv_core.Size(5, 5), 0);
+        Mat bgdModel = new Mat();
+        Mat fgdModel = new Mat();
 
-        // Krok 3: Progowanie binarne
-        Mat binary = new Mat();
-        opencv_imgproc.threshold(gray, binary, 100, 255, opencv_imgproc.THRESH_BINARY);
+        // Perform segmentation with GrabCut
+        opencv_imgproc.grabCut(image, mask, rect, bgdModel, fgdModel, 5, opencv_imgproc.GC_INIT_WITH_RECT);
 
-        // Krok 4: Wyszukiwanie konturów
-        MatVector contours = new MatVector();
-        Mat hierarchy = new Mat();
-        opencv_imgproc.findContours(binary, contours, hierarchy, opencv_imgproc.RETR_EXTERNAL, opencv_imgproc.CHAIN_APPROX_SIMPLE);
+        Mat foregroundMask = createForegroundMask(mask);
+        Mat foreground = new Mat(image.size(), opencv_core.CV_8UC3, new Scalar(0, 0, 0, 255));
 
-        // Krok 5: Utworzenie nowego obrazu z tymi dużymi obiektami
-        Mat result = Mat.zeros(image.size(), image.type()).asMat();
+        image.copyTo(foreground, foregroundMask);
+        return foreground;
+    }
 
-        for (int i = 0; i < contours.size(); i++) {
-            Mat contour = contours.get(i);
-            double area = opencv_imgproc.contourArea(contour);
-            if (area > 5000 && area < 50_000) {
-                Rect boundingBox = opencv_imgproc.boundingRect(contour);
-                Mat roi = new Mat(image, boundingBox);
-                roi.copyTo(new Mat(result, boundingBox));
+    private Mat getMaskForGrabCut(Mat image) {
+        return new Mat(image.size(), opencv_core.CV_8UC1, new Scalar(opencv_imgproc.GC_BGD));
+    }
+
+    private Rect getRectSurroundingObjectInTheCenterImage (Mat image) {
+        return new Rect(10, 10, image.cols() - 20, image.rows() - 20);
+    }
+
+    private Mat createForegroundMask(Mat mask) {
+        Mat foregroundMask = new Mat(mask.size(), opencv_core.CV_8UC1);
+        for (int y = 0; y < mask.rows(); y++) {
+            for (int x = 0; x < mask.cols(); x++) {
+                byte value = mask.ptr(y, x).get();
+                // We set 255 for foreground pixels (GC_FGD or GC_PR_FGD) and 0 for the rest
+                if (value == opencv_imgproc.GC_FGD || value == opencv_imgproc.GC_PR_FGD) {
+                    foregroundMask.ptr(y, x).put((byte) 255);
+                } else {
+                    foregroundMask.ptr(y, x).put((byte) 0);
+                }
             }
         }
-
-        return result;
+        return foregroundMask;
     }
 
     private Mat multipartFileToMat(MultipartFile file) throws IOException {
